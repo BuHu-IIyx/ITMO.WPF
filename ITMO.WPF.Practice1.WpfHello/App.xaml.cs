fd641c5b38d677de3cacc70f27a84b15e35c4312
Alexander Vasilenko <buhu.iiyxs@gmail.com>
2021-07-03 11:10:56 +0300

    03/07/2021

---
+++ ITMO.WPF.Practice1.WpfHello/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ITMO.WPF.Practice1.WpfHello
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
